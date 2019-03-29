fca12aef15bf938ef34664164ec78921a54eeb25
Dejan Ranisavljevic <dejan.ranisavljevic@gmail.com>
2019-03-29 14:29:00 +0100

    Update build and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,36 @@
-'use strict'
+'use strict';
 
-var gulp = require('gulp')
-  , sourcemaps = require('gulp-sourcemaps')
-  , rimraf = require('gulp-rimraf')
-  , less = require('gulp-less')
-  , gulpif = require('gulp-if')
-  , browserSync = require('browser-sync')
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const rimraf = require('gulp-rimraf');
+const gulpif = require('gulp-if');
+const isProduction = process.env.NODE_ENV === 'production';
 
-var isProduction = process.env.NODE_ENV === 'production'
+const clean = () => {
+  return gulp.src(['dist/styles/*.css', 'dist/font/*']).pipe(rimraf());
+};
 
-gulp.task('less', function() {
-  return gulp.src('./src/styles/*.less')
+const less = () => {
+  return gulp
+    .src('./src/styles/*.less')
     .pipe(gulpif(!isProduction, sourcemaps.init()))
-    .pipe(less({
-      paths: ['./src/styles/']
-    }))
+    .pipe(
+      require('gulp-less')({
+        paths: ['./src/styles/'],
+      }),
+    )
     .pipe(gulpif(!isProduction, sourcemaps.write()))
-    .pipe(gulp.dest('./dist/css/'))
-})
+    .pipe(gulp.dest('./dist/css/'));
+};
 
-gulp.task('fonts', function() {
-  return gulp.src('./src/fonts/**')
-    .pipe(gulp.dest('./dist/fonts/'))
-})
+const fonts = () => {
+  return gulp.src('./src/fonts/**').pipe(gulp.dest('./dist/fonts/'));
+};
 
-gulp.task('clean', function() {
-  return gulp.src([
-    'dist/styles/*.css'
-  , 'dist/font/*'
-  ]).pipe(rimraf())
-})
+const build = gulp.series(clean, less, fonts);
 
-gulp.task('watch', ['build'], function watchAssets() {
-  gulp.watch('src/styles/**/*.less', ['less'])
-})
-
-gulp.task('sync', ['watch'], function () {
-  browserSync({
-    server: {
-      baseDir: ['.']
-    , index: 'index.html'
-    }
-    , port: 3000
-    , files: [
-        'index.html'
-      , 'dist/css/**'
-      ]
-    , open: false
-    , online: false
-    , notify: false
-  })
-})
-
-gulp.task('build', ['clean', 'less', 'fonts'])
-gulp.task('default', ['build'])
+exports.clean = clean;
+exports.less = less;
+exports.fonts = fonts;
+exports.build = build;
+exports.default = build;
